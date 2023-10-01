43aeb0c72644fe243ddedd96f0b4dc215c8bc26a
Scot Pfuntner <spinatose@gmail.com>
2023-09-30 21:25:55 -0500

    added employeeoverview component

---
+++ Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanyPieShopHRM.App.Pages
+{
+	public partial class EmployeeOverview
+	{
+		public List<Employee>? Employees { get; set; } = default; 
+
+		public EmployeeOverview()
+		{
+			Employees = MockDataService.Employees;
+		}
+	}
+}
+
